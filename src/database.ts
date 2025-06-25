7a62599819f2d7b9b1bbcb72792e929e78125a96
lucaslima <lucaslima78@hotmail.com>
2025-06-25 07:28:50 -0300

    up

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,13 @@
 import {knex as Setupknex} from "knex";
-export const knex = Setupknex({
+export const config = {
     client:'sqlite',
     connection:{
         filename:'./tmp/app.db'
     },
+    migrations:{
+        directory:'./migrations',
+        extension:'ts'
+    },
     useNullAsDefault: true,
-})
+}
+export const knex = Setupknex(config)
